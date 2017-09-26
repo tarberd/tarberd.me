6075a1485aa3c2514d275d879ff41da1a6a753e3
tarberd <bernardo.mferrari@gmail.com>
2017-09-26 20:19:22 -0300

    hello world

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,17 @@
 import React from "react"
+import Typography from 'typography'
 
-export default () => <div>Hello world!</div>
+const typography = new Typography({ baseFontSize: '24px' })
+
+typography.injectStyles()
+
+class Index extends React.Component {
+    render() {
+        return (
+            <div style={{ margin: '3rem auto', maxWidth: 600 }}>
+                Welcome to my blog :)
+            </div>)
+    }
+}
+
+export default Index
