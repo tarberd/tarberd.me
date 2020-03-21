65aef54eafc061beafeef2c6b65840109558a891
tarberd <bernardo.mferrari@gmail.com>
2020-03-21 18:19:32 -0300

    Remove old website

--- README.md
+++
@@ -1,2 +0,0 @@
-# Bernardo Ferrari's Personal Blog
-Starter with the bare essentials needed for a Gatsby site
--- src/pages/typography/basic_typo.js
+++
@@ -1,8 +0,0 @@
-import Typography from 'typography'
-import fairyGateTheme from 'typography-theme-fairy-gates'
-
-const basic_typo = new Typography(fairyGateTheme)
-
-basic_typo.injectStyles()
-
-export default basic_typo
